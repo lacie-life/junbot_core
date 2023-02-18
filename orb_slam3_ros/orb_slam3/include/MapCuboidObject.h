63528e8a0b995d9a528aa6ea79b62f72e79c187d
lacie <00sao00ios00@gmail.com>
2023-02-18 11:57:18 +0700

    [3D Cuboid testing] nothing ...

---
+++ orb_slam3_ros/orb_slam3/include/MapCuboidObject.h
@@ -0,0 +1,14 @@
+//
+// Created by lacie on 18/02/2023.
+//
+
+#ifndef ORB_SLAM3_ROS_MAPCUBOIDOBJECT_H
+#define ORB_SLAM3_ROS_MAPCUBOIDOBJECT_H
+
+
+class MapCuboidObject {
+
+};
+
+
+#endif //ORB_SLAM3_ROS_MAPCUBOIDOBJECT_H
---
+++ orb_slam3_ros/orb_slam3/src/MapCuboidObject.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lacie on 18/02/2023.
+//
+
+#include "MapCuboidObject.h"
