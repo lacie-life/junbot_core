fb57714321b2d9a19c7648d68025382781661d08
lacie <00sao00ios00@gmail.com>
2023-03-08 00:28:59 +0700

    [Pending] Add TODO

--- junbot_adaptive/src/adaptiveControl.cpp
+++ junbot_adaptive/src/adaptiveControl.cpp
@@ -4,8 +4,17 @@
 
 #include <iostream>
 #include <ros/ros.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/Path.h>
+#include <tf/transform_listener.h>
 
-// TODO: Dev hehehehehehhehehe
+// TODO: Subscribe map infor
+ros::Subscriber m_plannerPathSub;
+ros::Subscriber map_sub;
+
+void mapCallback(nav_msgs::OccupancyGrid::ConstPtr map);
+void plannerPathCallback(nav_msgs::Path::ConstPtr path);
 
 int main(int argc, char** argv) {
 
