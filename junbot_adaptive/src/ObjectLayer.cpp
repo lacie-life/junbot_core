16e36673a42fe88196f12a26dc79c952c9e313a6
lacie <00sao00ios00@gmail.com>
2023-03-08 00:01:00 +0700

    [Adding] objectLayer node

--- junbot_adaptive/src/ObjectLayer.cpp
+++ junbot_adaptive/src/ObjectLayer.cpp
@@ -22,21 +22,55 @@ void ObjDBCallBack(const vision_msgs::Detection3DArray::ConstPtr& msg)
     {
         custom_msgs::Form obs;
 
+        //     8------7
+        //    /|     /|
+        //   / |    / |
+        //  5------6  |
+        //  |  4---|--3
+        //  | /    | /
+        //  1------2
+
         vision_msgs::Detection3D detect = msg->detections.at(i);
 
         // TODO: Convert vision_msgs::Detection3D to custom_msgs::Obstacles
-        for (int n = 0 ; n < 4; n++) {
-            geometry_msgs::Point p;
+        // TODO: Convert to map coordinate ?
 
-            obs.form.push_back(p);
-        }
-        temp.list.push_back(obs);
+        // 1
+        geometry_msgs::Point p1;
+        p1.x = detect.bbox.center.position.x - detect.bbox.size.x/2;
+        p1.y = detect.bbox.center.position.y + detect.bbox.size.y/2;
+        p1.z = 0.0;
+        obs.form.push_back(p1);
+
+        // 2
+        geometry_msgs::Point p2;
+        p2.x = detect.bbox.center.position.x + detect.bbox.size.x/2;
+        p2.y = detect.bbox.center.position.y + detect.bbox.size.y/2;
+        p2.z = 0.0;
+        obs.form.push_back(p2);
+
+        // 3
+        geometry_msgs::Point p3;
+        p2.x = detect.bbox.center.position.x + detect.bbox.size.x/2;
+        p2.y = detect.bbox.center.position.y - detect.bbox.size.y/2;
+        p2.z = 0.0;
+        obs.form.push_back(p3);
+
+        // 4
+        geometry_msgs::Point p4;
+        p2.x = detect.bbox.center.position.x - detect.bbox.size.x/2;
+        p2.y = detect.bbox.center.position.y - detect.bbox.size.y/2;
+        p2.z = 0.0;
+        obs.form.push_back(p4);
+
+        obs.id = detect.results.at(0).id;
+        obs.score = 1.0;
 
         temp.list.push_back(obs);
     }
     // Publish message msg to topic chatter
     obstaclePub.publish(temp);
- }
+}
 
 int main(int argc, char **argv)
 {
@@ -49,10 +83,10 @@ int main(int argc, char **argv)
     // Publisher is chatter_pub publishing to topic /object_costamp_layer/obsctacles with queue_size 1000
     ros::Publisher chatter_pub = nh.advertise<custom_msgs::Obstacles>("/object_costamp_layer/obsctacles", 1000);
     ros::Subscriber sub = nh.subscribe("detect_array", 1000, ObjDBCallBack);
+
     obstaclePub = nh.advertise<custom_msgs::Obstacles>("/object_costmap_layer/obstacles", 1000);
 
-    int  count = 0;
-    ros::Rate rate(1000);
+    ros::spin();
 
     return 0;
 }
