96384b5bccb56671290ed079ac6342832c91b6a4
HN29 <caohuynhat0209@gmail.com>
2023-03-13 22:35:30 +0700

    [update] fix subscriber waypoint

--- junbot_adaptive/src/subscribeTraj.cpp
+++ junbot_adaptive/src/subscribeTraj.cpp
@@ -13,41 +13,19 @@ ros::Publisher pub;
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
-void chatterCallback(nav_msgs::Path path)
+void globalPlanCallback(nav_msgs::Path path)
 {
     for (int i = 0; i < path.poses.size(); i++)
     {
-        tempTraj.poses[i].pose.position.x = path.poses[i].pose.position.x;
-        tempTraj.poses[i].pose.position.y = path.poses[i].pose.position.y;
-        ROS_INFO("%f", tempTraj.poses[i].pose.position.x );
+        tempTraj.poses.push_back(path.poses[i]);
+        std::cout << tempTraj.poses[i].pose.position.x << "\n";
     }
 }
 
 int main(int argc, char **argv)
 {
-    actionlib_msgs::GoalID tempCancel;
     ros::init(argc, argv, "listener");
     ros::NodeHandle n;
-    ros::Publisher  cancel = n.advertise<actionlib_msgs::GoalID>("/move_base/cancel", 1000);
-    ros::Rate loop_rate(10);
-    while (ros::ok())
-    {
-        int c = getchar();   // call your non-blocking input function
-        if (c == 'a')
-        {
-            ros::Subscriber sub = n.subscribe("/move_base/DWAPlannerROS/global_plan", 1000, chatterCallback);
-            tempCancel.stamp = {};
-            tempCancel.id = {};
-            cancel.publish(tempCancel);
-        }
-        else if (c == 'b')
-        {
-            pub = n.advertise<nav_msgs::Path>("/move_base/DWAPlannerROS/global_plan", 1000);
-            pub.publish(tempTraj);
-        }
-        ros::Rate loop_rate(10);
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
+    ros::Subscriber sub = n.subscribe("/move_base/DWAPlannerROS/global_plan", 10000, globalPlanCallback);
     return 0;
 }
