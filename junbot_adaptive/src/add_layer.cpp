2d4dbc59356226064a09dc9870fb6988aa34c7bf
HN29 <caohuynhat0209@gmail.com>
2023-03-13 22:34:43 +0700

    [update] add coner layer

--- junbot_adaptive/src/add_layer.cpp
+++ junbot_adaptive/src/add_layer.cpp
@@ -11,39 +11,58 @@ int main(int argc, char **argv)
     ros::Publisher chatter_pub = n.advertise<custom_msgs::Obstacles>("/object_costamp_layer/obsctacles", 1000);
 
     int  count = 0;
-    ros::Rate rate(1000);
+    ros::Rate rate(100);
     while (ros::ok())
     {
         // create message msg with date type is string
         std_msgs::String msg;
         // creat variable ss with data type is stringstream
-        float point_[8][3] = {{-3,1,0},
-                              {-4,1,0},
-                              {-3,3,0},
-                              {-4,3,0},
+        float point_[8][3] = {{-3,0.75,0},
+                              {-4,0.75,0},
+                              {-3,6,0},
+                              {-4,6,0},
                               {-1.5, 0, 0},
                               {-2, 0, 0},
                               {-1.5, 2, 0},
                               {-2, 2, 0}};
+        float circle_[8][3] = {{-3,0.75,0.15},
+                               {-4,0.75,0.15},
+                               {-3,6,0.15},
+                               {-4,6,0.15},
+                               {-1.5, 0, .15},
+                               {-2, 0, 0.15},
+                               {-1.5, 2, 0.15},
+                               {-2, 2, 0.15}};
+        int number_circle = 8;
         int number_objs = 2;
 
         custom_msgs::Obstacles temp;
-
+        // add object layer
         for (int i = 0; i < number_objs; i++)
         {
             custom_msgs::Form obs;
             for (int n = 0 ; n < 4; n++) {
                 geometry_msgs::Point p;
-                p.x = point_[n+i*4][0];
-                p.y = point_[n+i*4][1];
-                p.z = point_[n+i*4][2];
-
+                 p.x = point_[n+i*4][0];
+                 p.y = point_[n+i*4][1];
+                 p.z = point_[n+i*4][2];
                 obs.form.push_back(p);
             }
             temp.list.push_back(obs);
         }
-
-        // Publish message msg to topic chatter
+//        // add circle
+//        for (int i = 0; i < number_circle; i++)
+//        {
+//            custom_msgs::Form obs;
+//            for (int n = 0 ; n < 1; n++) {
+//                geometry_msgs::Point p;
+//                p.x = circle_[i][0];
+//                p.y = circle_[i][1];
+//                p.z = circle_[i][2];
+//                obs.form.push_back(p);
+//            }
+//            temp.list.push_back(obs);
+//        }
         chatter_pub.publish(temp);
         ros::spinOnce();
         ++count;
