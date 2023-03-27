5f50fd1cda51129de7f3bd0a9b3f4ac2c7c809b0
lacie <00sao00ios00@gmail.com>
2023-03-27 19:30:00 +0700

    [Add] add detector

--- object_mapping/include/Detector.h
+++ object_mapping/include/Detector.h
@@ -21,21 +21,21 @@
 
 using namespace nvinfer1;
 
+static std::vector<sl::uint2> cvt(const cv::Rect &bbox_in){
+    std::vector<sl::uint2> bbox_out(4);
+    bbox_out[0] = sl::uint2(bbox_in.x, bbox_in.y);
+    bbox_out[1] = sl::uint2(bbox_in.x + bbox_in.width, bbox_in.y);
+    bbox_out[2] = sl::uint2(bbox_in.x + bbox_in.width, bbox_in.y + bbox_in.height);
+    bbox_out[3] = sl::uint2(bbox_in.x, bbox_in.y + bbox_in.height);
+    return bbox_out;
+}
+
 class Detector
 {
 public:
     Detector(const std::string _model_path);
     ~Detector();
-    std::vector<Yolo::Detection> detectObject(const cv::Mat& _frame, std::vector<Yolo::Detection>& objects);
-
-    std::vector<sl::uint2> cvt(const cv::Rect &bbox_in){
-        std::vector<sl::uint2> bbox_out(4);
-        bbox_out[0] = sl::uint2(bbox_in.x, bbox_in.y);
-        bbox_out[1] = sl::uint2(bbox_in.x + bbox_in.width, bbox_in.y);
-        bbox_out[2] = sl::uint2(bbox_in.x + bbox_in.width, bbox_in.y + bbox_in.height);
-        bbox_out[3] = sl::uint2(bbox_in.x, bbox_in.y + bbox_in.height);
-        return bbox_out;
-    }
+    std::vector<Yolo::Detection> detectObject(const cv::Mat& _frame);
 
 private:
     ICudaEngine* build_engine(unsigned int maxBatchSize, IBuilder* builder, IBuilderConfig* config, DataType dt, float& gd, float& gw, std::string& wts_name);
--- object_mapping/src/main.cpp
+++ object_mapping/src/main.cpp
@@ -6,6 +6,7 @@
 
 #include "GLViewer.hpp"
 #include "Detector.h"
+#include "yololayer.h"
 
 int main(int argc, char** argv) {
 
@@ -58,14 +59,64 @@ int main(int argc, char** argv) {
     sl::Pose cam_w_pose;
     cam_w_pose.pose_data.setIdentity();
 
+    int frameCount = 0;
+
     while (viewer.isAvailable())
     {
         if(zed.grab() == sl::ERROR_CODE::SUCCESS)
         {
             // TODO: Process image
+            zed.retrieveImage(left_sl, sl::VIEW::LEFT);
 
+            // Preparing inference
+            cv::Mat left_cv_rgba = slMat2cvMat(left_sl);
+            cv::cvtColor(left_cv_rgba, left_cv_rgb, cv::COLOR_BGRA2BGR);
+
+            if (left_cv_rgb.empty())
+            {
+                continue;
+            }
+
+            std::vector<Yolo::Detection> objs = detector->detectObject(left_cv_rgb);
+
+            std::cout << "Number object in frame " << frameCount << "th: " << objs.size();
+
+            // Preparing for ZED SDK ingesting
+            std::vector<sl::CustomBoxObjectData> objects_in;
+            for (auto &it : objs) {
+                sl::CustomBoxObjectData tmp;
+                cv::Rect r = get_rect(left_cv_rgb, it.bbox);
+                // Fill the detections into the correct format
+                tmp.unique_object_id = sl::generate_unique_id();
+                tmp.probability = it.conf;
+                tmp.label = (int) it.class_id;
+                tmp.bounding_box_2d = cvt(r);
+                tmp.is_grounded = ((int) it.class_id == 0); // Only the first class (person) is grounded, that is moving on the floor plane
+                // others are tracked in full 3D space
+                objects_in.push_back(tmp);
+            }
+            // Send the custom detected boxes to the ZED
+            zed.ingestCustomBoxObjects(objects_in);
+
+            // Displaying 'raw' objects
+            for (size_t j = 0; j < objs.size(); j++) {
+                cv::Rect r = get_rect(left_cv_rgb, objs[j].bbox);
+                cv::rectangle(left_cv_rgb, r, cv::Scalar(0x27, 0xC1, 0x36), 2);
+                cv::putText(left_cv_rgb, std::to_string((int) objs[j].class_id), cv::Point(r.x, r.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+            }
+            cv::imshow("Objects", left_cv_rgb);
+            cv::waitKey(10);
+
+            // Retrieve the tracked objects, with 2D and 3D attributes
+            zed.retrieveObjects(objects, objectTracker_parameters_rt);
+            // GL Viewer
+            zed.retrieveMeasure(point_cloud, sl::MEASURE::XYZRGBA, sl::MEM::GPU, pc_resolution);
+            zed.getPosition(cam_w_pose, sl::REFERENCE_FRAME::WORLD);
+            viewer.updateData(point_cloud, objects.object_list, cam_w_pose.pose_data);
         }
     }
+
+    viewer.exit();
 
     return 0;
 }
