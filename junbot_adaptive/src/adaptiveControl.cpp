ba37c3a645b83c12b2fa26e1881cb991d516d1b6
HN29 <caohuynhat0209@gmail.com>
2023-03-29 20:56:34 +0700

    [update] add layer in experiment

--- junbot_adaptive/src/adaptiveControl.cpp
+++ junbot_adaptive/src/adaptiveControl.cpp
@@ -61,12 +61,18 @@ int main(int argc, char **argv) {
     ros::Subscriber subPlan = n.subscribe("/move_base/DWAPlannerROS/global_plan", 10000, globalPlanCallback);
     ros::Subscriber subObj = n.subscribe("/object_costmap_layer/obsctacles_temp", 10000, objectCallback);
     ros::Publisher pubObj = n.advertise<custom_msgs::Obstacles>("/object_costmap_layer/obsctacles", 1000);
-    float coner[4][3] = {{-0.1,0.8,0},
-                            {2.3,0.8,0},
-                            {-5,2.3,0},
-                            {2.35,4.15,0}};
+    //coner simulation
+    // float coner[4][3] = {{-0.1,0.8,0},
+    //                         {2.3,0.8,0},
+    //                         {-5,2.3,0},
+    //                         {2.35,4.15,0}};
                             // {-6.3, 0.85, 0}
+    // coner experiment
     ros::Rate rate(100);
+    // float coner[4][3] = {{0.22,-0.6,0},
+    //                      {0.22,-1.8,0},
+    //                      {-3.6,-1.7,0},
+    //                      {-3.6,-0.35,0}};
     while (ros::ok())
     {
         m.lock();
@@ -98,33 +104,33 @@ int main(int argc, char **argv) {
                 }
             }
         }
-        for (int j = 0; j < 4; ++j) {
-            std::vector<geometry_msgs::Point> waypointError;
-            custom_msgs::Form temp;
-            for (int k = 0; k < path.poses.size(); ++k) {
-                distance = calculateDistance(coner[j][0],coner[j][1],path.poses[k].pose.position.x, path.poses[k].pose.position.y);                    if (distance <= 0.3) {
-                geometry_msgs::Point p;
-                p.x = path.poses[k].pose.position.x;
-                p.y = path.poses[k].pose.position.y;
-                p.z = path.poses[k].pose.position.z;
-                waypointError.push_back(p);
-                }
-            }
-            if (waypointError.size()>1)
-            {
-                geometry_msgs::Point _begin = waypointError.at(0);
-                geometry_msgs::Point _end = waypointError.at(waypointError.size() - 1);
-                geometry_msgs::Point coner_;
-                coner_.x = coner[j][0];
-                coner_.y = coner[j][1];
-                coner_.z = coner[j][2];
-                temp.form.push_back(_begin);
-                temp.form.push_back(_end);
-                temp.form.push_back(coner_);
-                temp.id = "zone";
-                objectNew.list.push_back(temp);
-            }
-        }
+        // for (int j = 0; j < 4; ++j) {
+        //     std::vector<geometry_msgs::Point> waypointError;
+        //     custom_msgs::Form temp;
+        //     for (int k = 0; k < path.poses.size(); ++k) {
+        //         distance = calculateDistance(coner[j][0],coner[j][1],path.poses[k].pose.position.x, path.poses[k].pose.position.y);                    if (distance <= 0.3) {
+        //         geometry_msgs::Point p;
+        //         p.x = path.poses[k].pose.position.x;
+        //         p.y = path.poses[k].pose.position.y;
+        //         p.z = path.poses[k].pose.position.z;
+        //         waypointError.push_back(p);
+        //         }
+        //     }
+        //     if (waypointError.size()>1)
+        //     {
+        //         geometry_msgs::Point _begin = waypointError.at(0);
+        //         geometry_msgs::Point _end = waypointError.at(waypointError.size() - 1);
+        //         geometry_msgs::Point coner_;
+        //         coner_.x = coner[j][0];
+        //         coner_.y = coner[j][1];
+        //         coner_.z = coner[j][2];
+        //         temp.form.push_back(_begin);
+        //         temp.form.push_back(_end);
+        //         temp.form.push_back(coner_);
+        //         temp.id = "zone";
+        //         objectNew.list.push_back(temp);
+        //     }
+        // }
         pubObj.publish(objectNew);
         m.unlock();
         ros::spinOnce();
