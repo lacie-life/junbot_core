f173e5bd511d2697acdc8299e788a37f5b04fcc1
lacie <00sao00ios00@gmail.com>
2023-07-17 21:50:52 +0700

    [Update] Add SemanticPlanner

--- junbot_adaptive/src/junbot_planner.cpp
+++ junbot_adaptive/src/junbot_planner.cpp
@@ -3,10 +3,19 @@
 //
 
 #include <iostream>
+#include <ros/ros.h>
+#include "SemanticPlanner.h"
 
-int main()
+int main(int argc, char** argv)
 {
     std::cout << "Hello, World!" << std::endl;
+
+    ros::init(argc, argv, "junbot_planner");
+
+    ros::NodeHandle nh;
+
+    SemanticPlanner semanticPlanner(nh);
+    semanticPlanner.run();
+
     return 0;
 }
-
