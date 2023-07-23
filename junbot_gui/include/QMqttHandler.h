f1d631b426be85e3e0e1334dd1fe33151f3efe0d
lacie <00sao00ios00@gmail.com>
2023-07-23 18:40:58 +0700

    [Update] add QMqttHandler

---
+++ junbot_gui/include/QMqttHandler.h
@@ -0,0 +1,73 @@
+#ifndef QMQTT_HANDLER_H
+#define QMQTT_HANDLER_H
+
+#include <QObject>
+#include <QString>
+#include <QtMqtt/QtMqtt>
+#include <QtMqtt/QMqttClient>
+#include <QVector>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QDebug>
+#include <QFile>
+#include <QMutex>
+
+struct RobotNode {
+    QString name;
+    QString ip;
+    QString current_state_topic;
+    QJsonObject current_state_message; // TODO: Which state need to publish for mobile app ? 
+    QString control_topic;
+    QJsonObject control_message; // TODO: How to create control message ? Which feature need to control ?
+};
+
+class QMqttHandler : public QObject
+{
+    Q_OBJECT
+
+    static QMqttHandler *m_instance;
+
+public:
+    static QMqttHandler *getInstance();
+    ~QMqttHandler();
+
+    bool loadMQTTSetting(QString path); // Load MQTT topics
+    int initBokerHost(QString path);
+    void connectMQTT(QString brokerName, qint16 port);
+
+    QString mqttMessage() const;
+    RobotNode currentRobotNode() const;
+
+public slots:
+    void onMQTT_Connected();
+    void onMQTT_disconnected();
+    void onMQTT_Received(const QByteArray &message, const QMqttTopicName &topic);
+
+    void MQTT_Publish(RobotNode node, QJsonObject message);
+    void MQTT_Subcrib(RobotNode node);
+
+    void setMqttMessage(QJsonObject &msg);
+    void setCurrentRobotNode(RobotNode node);
+
+signals:
+    void MQTT_Received(QString msg);
+    void mqttMessageChanged(QString msg);
+
+public:
+    QVector<RobotNode> RobotNodes;
+
+    QString device_path;
+    QString broker_path;
+
+private:
+    QMqttHandler(QObject* parent = nullptr);
+    QMqttHandler(const QMqttHandler&) = delete;
+    void operator =(const QMqttHandler&) = delete;
+
+    QMqttClient *m_client;
+    RobotNode m_current_robot_node;
+    QString m_mqttMessage;
+};
+
+#endif // QMQTT_HANDLER_H
---
+++ junbot_gui/src/QMqttHandler.cpp
@@ -0,0 +1,88 @@
+#include "QMqttHandler.h"
+#include "AppConstants.h"
+
+QMqttHandler *QMqttHandler::m_instance = nullptr;
+
+QMqttHandler::QMqttHandler(QObject* parent)
+    : QObject(parent)
+{
+}
+
+QMqttHandler *QMqttHandler::getInstance()
+{
+    if(m_instance == nullptr)
+    {
+        m_instance = new QMqttHandler();
+    }
+    return m_instance;
+}
+
+QMqttHandler::~QMqttHandler()
+{
+
+}
+
+bool QMqttHandler::loadMQTTSetting(QString path)
+{
+    return true;
+}
+
+int QMqttHandler::initBokerHost(QString path)
+{
+    broker_path = path;
+
+    return 0;
+}
+
+void QMqttHandler::connectMQTT(QString brokerName, qint16 port)
+{
+    m_client = new QMqttClient(this);
+
+    m_client->setHostname(brokerName);
+    m_client->setPort(port);
+
+    m_client->connectToHost();
+
+    connect(m_client, &QMqttClient::connected, this, &QMqttHandler::onMQTT_Connected);
+    connect(m_client, &QMqttClient::disconnected, this, &QMqttHandler::onMQTT_disconnected);
+    connect(m_client, &QMqttClient::messageReceived, this, &QMqttHandler::onMQTT_Received);
+}
+
+void QMqttHandler::onMQTT_Connected()
+{
+    CONSOLE << "Connected to MQTT Broker";
+    MQTT_Subcrib(m_current_robot_node);
+}
+
+void QMqttHandler::onMQTT_disconnected()
+{
+    CONSOLE << "Disconnected to MQTT Broker";
+}
+
+void QMqttHandler::onMQTT_Received(const QByteArray &message, const QMqttTopicName &topic)
+{
+    CONSOLE << "Received message: " << message << " from topic: " << topic.name();
+
+    QJsonObject msg = QJsonDocument::fromJson(message).object();
+
+    setMqttMessage(msg);
+
+    emit MQTT_Received(m_mqttMessage);
+}
+
+void QMqttHandler::MQTT_Publish(RobotNode node, QJsonObject message)
+{
+    QMqttTopicName topic(node.current_state_topic);
+
+    m_client->publish(topic, QJsonDocument(message).toJson());
+}
+
+void QMqttHandler::MQTT_Subcrib(RobotNode node)
+{
+    QMqttTopicFilter filter(node.control_topic);
+
+    m_client->subscribe(filter);
+}
+
+
+
